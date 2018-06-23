0ea42baebeab4f19723614de256c09ece806753b
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-06-24 03:36:55 +0600

    Basics more

---
+++ Basics/Functions.py
@@ -0,0 +1,8 @@
+# Using built in functions
+arr = [1.76,4.32,123,32,23.4]
+print(max(arr))
+
+rounded = round(arr[0] , 1)
+rounded = round(arr[0]) # rounds to closest int
+
+## using other built - in functions
